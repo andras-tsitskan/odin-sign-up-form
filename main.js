efacbca7650733541d3409ca79d307bab92c02a4
Andraš Tšitškan <36357686+andras-tsitskan@users.noreply.github.com>
2022-07-20 22:00:33 +0300

    Add password validation

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 "use strict";
+
+const passwordOriginalInput = document.querySelector(".js-password-original");
+const passwordConfirmInput = document.querySelector(".js-password-confirm");
+
+passwordConfirmInput.addEventListener("input", comparePasswords);
+
+function comparePasswords(event) {
+  const password = passwordOriginalInput.value;
+
+  if (password !== event.target.value) {
+    event.target.classList.add("error");
+  } else {
+    event.target.classList.remove("error");
+  }
+}
